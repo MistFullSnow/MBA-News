03ef8075bccd1d8c9ddeb443269ed1f797b43da5
Aryan Yede <aryan.yede.7@gmail.com>
2025-12-31 18:29:01 +0530

    Initial Android project with Gradle wrapper

---
+++ app/src/main/java/com/aryan/mbanews/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.aryan.cetreader
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("CET Reader – Setup Successful")
+        }
+    }
+}

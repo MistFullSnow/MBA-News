4df6918a8f741516b10b8d68095fc5e8e2ba2fd3
MistFullSnow <76036973+MistFullSnow@users.noreply.github.com>
2025-12-31 20:46:37 +0530

    Update HomeScreen.kt

--- app/src/main/java/com/aryan/cetreader/ui/HomeScreen.kt
+++ app/src/main/java/com/aryan/cetreader/ui/HomeScreen.kt
@@ -1,25 +1,18 @@
 package com.aryan.cetreader.ui
 
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.aryan.cetreader.ui.theme.AppTheme
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.LaunchedEffect
-import com.aryan.cetreader.ui.model.Article
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.aryan.cetreader.ui.theme.AppTheme
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable

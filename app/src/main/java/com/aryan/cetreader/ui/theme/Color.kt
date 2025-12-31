c18f438d298aea550776e452eead7ec2cbc458d5
MistFullSnow <76036973+MistFullSnow@users.noreply.github.com>
2025-12-31 19:21:28 +0530

    Create Color.kt

---
+++ app/src/main/java/com/aryan/cetreader/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.aryan.cetreader.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light
+val LightBg = Color(0xFFF7F9FC)
+val LightCard = Color.White
+val LightText = Color(0xFF111827)
+
+// Dark
+val DarkBg = Color(0xFF0F172A)
+val DarkCard = Color(0xFF111827)
+val DarkText = Color(0xFFE5E7EB)
+
+// AMOLED
+val AmoledBg = Color.Black
+val AmoledCard = Color(0xFF090909)
+val AmoledText = Color.White

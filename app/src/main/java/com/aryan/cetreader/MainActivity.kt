c136f4d25371540a24f5a32168e472f194a6c4bb
MistFullSnow <76036973+MistFullSnow@users.noreply.github.com>
2025-12-31 19:22:26 +0530

    Update MainActivity.kt

--- app/src/main/java/com/aryan/cetreader/MainActivity.kt
+++ app/src/main/java/com/aryan/cetreader/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.aryan.cetreader.ui.HomeScreen
+import com.aryan.cetreader.ui.theme.AppTheme
+import com.aryan.cetreader.ui.theme.CETReaderTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HomeScreen()
+            CETReaderTheme(theme = AppTheme.LIGHT) {
+                HomeScreen()
+            }
         }
     }
 }

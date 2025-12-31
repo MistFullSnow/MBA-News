0aae26c834fa80a70735f70f9dfcb81dac3a7ed9
MistFullSnow <76036973+MistFullSnow@users.noreply.github.com>
2025-12-31 19:24:14 +0530

    Update FeaturedCard.kt

--- app/src/main/java/com/aryan/cetreader/ui/FeaturedCard.kt
+++ app/src/main/java/com/aryan/cetreader/ui/FeaturedCard.kt
@@ -1,5 +1,9 @@
 package com.aryan.cetreader.ui
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Card
@@ -7,29 +11,37 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FeaturedCard() {
-    Card(
-        modifier = Modifier
-            .padding(12.dp)
-            .fillMaxWidth()
-            .height(140.dp)
-            .clickable { },
-        elevation = CardDefaults.cardElevation(6.dp),
-        shape = CardDefaults.shape
+
+    AnimatedVisibility(
+        visible = true,
+        enter = fadeIn(tween(500)) + slideInVertically(
+            initialOffsetY = { -it / 4 }
+        )
     ) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text("Featured", fontWeight = FontWeight.SemiBold)
-            Spacer(modifier = Modifier.height(6.dp))
-            Text(
-                "Elon Musk says X will pay content creators more than YouTube",
-                fontWeight = FontWeight.Bold
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-            Text("Source: Times of India • 1h ago")
+        Card(
+            modifier = Modifier
+                .padding(12.dp)
+                .fillMaxWidth()
+                .height(140.dp)
+                .clickable { },
+            elevation = CardDefaults.cardElevation(6.dp),
+            shape = CardDefaults.shape
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text("Featured", fontWeight = FontWeight.SemiBold)
+                Spacer(modifier = Modifier.height(6.dp))
+                Text(
+                    "Elon Musk says X will pay content creators more than YouTube",
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text("Source: Times of India • 1h ago")
+            }
         }
     }
 }

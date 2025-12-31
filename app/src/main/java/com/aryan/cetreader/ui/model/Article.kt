7926f7daea6226840ef6964dc3ee83702f087ded
MistFullSnow <76036973+MistFullSnow@users.noreply.github.com>
2025-12-31 20:15:18 +0530

    Create Article.kt

---
+++ app/src/main/java/com/aryan/cetreader/ui/model/Article.kt
@@ -0,0 +1,9 @@
+package com.aryan.cetreader.ui.model
+
+data class Article(
+    val title: String,
+    val description: String,
+    val link: String,
+    val source: String,
+    val pubDate: String
+)

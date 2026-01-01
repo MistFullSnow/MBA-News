13dc365300dd777f3614f2aae6ee7fa60d073daf
MistFullSnow <76036973+MistFullSnow@users.noreply.github.com>
2026-01-01 18:11:08 +0530

    Update HomeViewModel.kt

--- app/src/main/java/com/aryan/cetreader/ui/HomeViewModel.kt
+++ app/src/main/java/com/aryan/cetreader/ui/HomeViewModel.kt
@@ -2,23 +2,22 @@ package com.aryan.cetreader.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.aryan.cetreader.ui.model.Article
-import com.aryan.cetreader.ui.rss.RssRepository
-import kotlinx.coroutines.Dispatchers
+import com.aryan.cetreader.data.NewsRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
 
-    private val repository = RssRepository()
+    private val repository = NewsRepository()
 
     private val _articles = MutableStateFlow<List<Article>>(emptyList())
     val articles: StateFlow<List<Article>> = _articles
 
     fun loadArticles(rssUrl: String) {
-    viewModelScope.launch {
-        val data = repository.fetchArticles(rssUrl)
-        _articles.emit(data)
+        viewModelScope.launch {
+            val data = repository.fetchArticles(rssUrl)
+            _articles.value = data
+        }
     }
 }

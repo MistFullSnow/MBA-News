c89b2774071eaac5fe7b604a5b12164e98f67b07
MistFullSnow <76036973+MistFullSnow@users.noreply.github.com>
2026-01-01 18:10:40 +0530

    Update HomeScreen.kt

--- app/src/main/java/com/aryan/cetreader/ui/HomeScreen.kt
+++ app/src/main/java/com/aryan/cetreader/ui/HomeScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp 
 import androidx.lifecycle.viewmodel.compose.viewModel 
 import com.aryan.cetreader.ui.theme.AppTheme
+import androidx.compose.material.icons.filled.ArrowBack
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable

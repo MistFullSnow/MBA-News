9970f7fbbbdb88ffb0dde65f8f522886494ce0e6
MistFullSnow <76036973+MistFullSnow@users.noreply.github.com>
2026-01-01 17:11:21 +0530

    Update HomeScreen.kt

--- app/src/main/java/com/aryan/cetreader/ui/HomeScreen.kt
+++ app/src/main/java/com/aryan/cetreader/ui/HomeScreen.kt
@@ -1,3 +1,18 @@
+package com.aryan.cetreader.ui 
+
+import androidx.compose.foundation.layout.* 
+import androidx.compose.foundation.lazy.grid.GridCells 
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid 
+import androidx.compose.foundation.lazy.grid.items 
+import androidx.compose.material.icons.Icons 
+import androidx.compose.material.icons.filled.MoreVert 
+import androidx.compose.material3.* 
+import androidx.compose.runtime.* 
+import androidx.compose.ui.Modifier 
+import androidx.compose.ui.unit.dp 
+import androidx.lifecycle.viewmodel.compose.viewModel 
+import com.aryan.cetreader.ui.theme.AppTheme
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(

d2327825cdb29562e908387a8ecf30338629abb1
MistFullSnow <76036973+MistFullSnow@users.noreply.github.com>
2026-01-01 18:39:45 +0530

    Update Article.kt

--- app/src/main/java/com/aryan/cetreader/ui/model/Article.kt
+++ app/src/main/java/com/aryan/cetreader/ui/model/Article.kt
@@ -2,8 +2,7 @@ package com.aryan.cetreader.ui.model
 
 data class Article(
     val title: String,
-    val description: String,
-    val link: String,
     val source: String,
-    val pubDate: String
+    val pubDate: String,
+    val link: String
 )
